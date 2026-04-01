3864a49bc2469476c84ae25b2d1e1ffe1815f2a4
cbruegg <mail@cbruegg.com>
2026-04-01 19:09:13 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+import { Codex } from "@openai/codex-sdk";
+
+const codex = new Codex();
+const thread = codex.startThread();
+const turn = await thread.run("Just reply with hello world");
+
+console.log(turn.finalResponse);
+console.log(turn.items);
